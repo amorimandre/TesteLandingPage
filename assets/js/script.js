bcdbe0924d4a629bc0f8542f84c7304fae08c5b0
André Amorim <andre.pereira0220@gmail.com>
2023-08-14 13:08:28 -0300

    js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let showingPage = 1;
+let leftBttn = document.getElementById("bttn-left");
+let rightBttn = document.getElementById("bttn-right");
+let cardPedro = document.getElementById("card-pedro");
+let cardHerick = document.getElementById("card-herick");
+let testArea = document.getElementById("testimonials-area");
